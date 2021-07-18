9647dbe2dba22aa1ef6e3b77d04d75d333fab3ad
Austin Hunt <huntaj@g.cofc.edu>
2021-07-18 18:17:38 -0400

    auto-provisioning of new brokers,update readme
    update readme, fix testing skip conditions
    
    update readme
    
    update readme

--- src/unit_tests/test_publisher.py
+++ src/unit_tests/test_publisher.py
@@ -1,11 +1,13 @@
 """ Module to perform unit tests against Publisher class for methods that
 execute and can be tested independently of the publish/subscribe network """
+from src.lib.zookeeper_client import ZookeeperClient
 import unittest
 import time
 import pickle
 from src.unit_tests import *
 from src.lib.publisher import Publisher
-
+import sys
+connected = False
 class TestPublisher(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         self.topics = ['A','B','C']
@@ -13,7 +15,28 @@ class TestPublisher(unittest.TestCase):
             topics=self.topics,
             sleep_period=0.3,
         )
+        try:
+            global connected
+            zk = ZookeeperClient(zookeeper_hosts=['127.0.0.1:2181'],use_logger=True)
+            zk.connect_zk()
+            zk.start_session()
+            zk.stop_session()
+            zk.close_connection()
+            connected = True
+        except Exception as e:
+            print(e)
+            print("You need to start the ZooKeeper service (port 2181) before running these tests")
+            sys.exit(1)
         super(TestPublisher, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.zookeeper_client = ZookeeperClient(zookeeper_hosts=['127.0.0.1:2181'],use_logger=True)
+        self.zookeeper_client.connect_zk()
+        self.zookeeper_client.start_session()
+
+    def tearDown(self):
+        self.zookeeper_client.stop_session()
+        self.zookeeper_client.close_connection()
 
     def test_topics(self):
         # Publisher should store all topics passed on construction.
@@ -29,18 +52,8 @@ class TestPublisher(unittest.TestCase):
         # Default port is 5556
         assert self.publisher.bind_port == 5556
 
-    def test_generate_publish_event(self):
 
-        # Only 3 topics, iteration 4 will use topic index 1 (B)
-        iteration = 4
-        event = self.publisher.generate_publish_event(iteration=iteration)
-        compare_topic = self.topics[iteration % len(self.topics)]
-        compare_topic_encoded = compare_topic.encode('utf8')
-        pickled_event_dict = event[1]
-        unpickled_event_dict = pickle.loads(pickled_event_dict)
-        assert unpickled_event_dict['publisher'] == '127.0.0.1:5556'
-        assert unpickled_event_dict['topic'] == compare_topic
-        assert event[0] == b'%b' % compare_topic_encoded
-        assert (time.time() - unpickled_event_dict['publish_time']) < 2
+
+
 
 
